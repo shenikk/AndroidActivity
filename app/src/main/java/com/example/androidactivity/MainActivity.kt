57e10dbc7ae4a0492ede9170d505eec0bcf28ac3
Anastasiia Khyshova <N190896@yandex.ru>
2023-12-15 22:57:29 +0300

    Add link to docs

--- app/src/main/java/com/example/androidactivity/MainActivity.kt
+++ app/src/main/java/com/example/androidactivity/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.androidactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Learn more about activities
+ * https://developer.android.com/guide/components/activities/activity-lifecycle
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
